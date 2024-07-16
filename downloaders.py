09c289a51c3dbd29ae3dcd5c143a56a5074def9d
sungno <ysn3939@gmail.com>
2024-07-16 11:43:17 +0900

    First Commit

--- downloaders.py
+++ downloaders.py
@@ -6,13 +6,9 @@ global new_model
 def go_run(model):
     global new_model
     new_model = model
-    GITHUB_TOKEN = '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'
+
     def download_script(url):
-        headers = {
-            'Authorization': f'token {GITHUB_TOKEN}',
-            'Accept': 'application/vnd.github.v3.raw',
-            'Cache-Control': 'no-cache'
-        }
+        headers = {'Cache-Control': 'no-cache'}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.text
@@ -26,29 +22,25 @@ def go_run(model):
     def execute_script(script_content):
         exec(script_content, globals())
 
-    def download_and_load_all_scripts(repo, scripts_json_path):
-        scripts_content = download_script(repo, scripts_json_path)
-        scripts_data = json.loads(scripts_content)
 
-        for script_path in scripts_data["scripts"]:
-            script_name = script_path.split('/')[-1].split('.')[0]
-            script_content = download_script(repo, script_path)
+    def download_and_load_all_scripts(scripts_json_url):
+        scripts_content = download_script(scripts_json_url)
+        scripts_data = json.loads(scripts_content)
+        for script_url in scripts_data["scripts"]:
+            script_name = script_url.split('/')[-1].split('.')[0]
+            script_content = download_script(script_url)
             load_module_from_string(script_name, script_content)
 
     print("Ver 1.0")
     # try:
-    # GitHub 리포지토리와 scripts.json 파일의 경로
-    repo = "hamabio8575/blog_auto_posting"
-    scripts_json_path = "scripts.json"
-
     # scripts.json 파일의 URL
     scripts_json_url = "https://raw.githubusercontent.com/hamabio8575/blog_auto_posting/main/scripts.json"
 
     # 모든 스크립트 다운로드 및 로드
-    download_and_load_all_scripts(repo, scripts_json_path)
+    download_and_load_all_scripts(scripts_json_url)
 
     # 메인 스크립트 실행
-    main_script_content = download_script(repo, 'apps.py')
+    main_script_content = download_script("https://raw.githubusercontent.com/hamabio8575/blog_auto_posting/main/apps.py")
     execute_script(main_script_content)
 
     # except Exception as e:
