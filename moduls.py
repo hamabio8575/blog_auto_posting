641bbf56c4c7010584d8aa8d5b975dba7a06bd21
sungno <ysn3939@gmail.com>
2024-07-16 10:27:25 +0900

    First Commit

---
+++ moduls.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from PyQt5.QtGui import QIcon
+from PyQt5.Qt import *
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+import threading
+import pandas as pd
+import numpy as np
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from fake_useragent import UserAgent
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pyperclip
+import pyautogui
+
+import requests
+import os
+import random
+from datetime import datetime
+
+import pywinauto
+from pywinauto import application
+import autoit
+import logging
+
+# print(pywinauto.__version__) # 0.6.3
+#
+# import inspect
+# print(inspect.getfile(pywinauto))
