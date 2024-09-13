2cbc99b92891cfe5c249c562429e028e95b8f954
sungno <ysn3939@gmail.com>
2024-09-13 14:43:52 +0900

    update video link, img slide

--- posting_utils.py
+++ posting_utils.py
@@ -331,10 +331,10 @@ def posting_run(write_contents, image_folder_path, img_file_len, video_folder_pa
                     # 파일 탐색기 열기 대화상자에 포커스를 맞춤
                     autoit.control_focus("열기", "")
                     time.sleep(1)
-                    multi_img_folder_path =rf"{multi_img_folder_path}\{multi_img_cnt}"
+                    multi_img_folder_path_cnt =rf"{multi_img_folder_path}\{multi_img_cnt}"
                     # 탐색기 주소 창에 원하는 폴더 경로를 입력
                     print(multi_img_folder_path)
-                    autoit.control_set_text("열기", "Edit1", multi_img_folder_path)
+                    autoit.control_set_text("열기", "Edit1", multi_img_folder_path_cnt)
                     time.sleep(1)
                     autoit.send("{ENTER}")
                     time.sleep(1)
