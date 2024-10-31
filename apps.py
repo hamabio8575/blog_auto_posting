ea9015bd4fda75449e9ec7eaa9c8089d35d156eb
sungno <ysn3939@gmail.com>
2024-10-31 13:04:47 +0900

    mvpn to autoit update

--- apps.py
+++ apps.py
@@ -138,185 +138,186 @@ while True:
             input("mvpn 접속 에러... 현재상태 캡쳐 부탁드리고, 터미널창에 엔터 입력후 ui가 종료되는지 체크 부탁드립니다.")
         time.sleep(5)
 
-        ### 네이버 로그인
-        try:
-            headless_checked = False
-            try:
-                driver = driver_utils.driversetting(headless_checked)
-                print("driver 세팅 완료")
-            except:
-                print("☆ Error driver setting...")
-                print("30초후 재개 됩니다.")
-                time.sleep(30)
-                driver = driver_utils.driversetting(headless_checked)
-
-            driver.maximize_window()
-            wait = WebDriverWait(driver, 10)
-            try:
-                driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-            except:
-                print("□ 드라이버 오픈 에러.. 30초후 재시작합니다.")
-                time.sleep(30)
-                try:
-                    driver.close()
-                    time.sleep(5)
-                    driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-                except:
-                    driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-            time.sleep(1)
-            wait, driver = driver_utils.naver_login(driver, wait, naver_id, naver_pw)  # 로그인 함수
-
-            # 블로그 클릭
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'service_icon.type_blog'))).click()
-            time.sleep(1)
-            # 글쓰기 클릭
-            driver.switch_to.window(driver.window_handles[-1])
-            wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
-            time.sleep(3)
-
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.switch_to.default_content()  # 기본 iframe으로 복귀
-            driver.switch_to.frame('mainFrame')
-        except:
-            try:
-                driver_utils.driver_close(driver)
-            except:
-                pass
-
-            headless_checked = False
-            try:
-                driver = driver_utils.driversetting(headless_checked)
-                print("driver 세팅 완료")
-            except:
-                print("☆ Error driver setting...")
-                print("30초후 재개 됩니다.")
-                time.sleep(30)
-                driver = driver_utils.driversetting(headless_checked)
-
-            driver.maximize_window()
-            wait = WebDriverWait(driver, 10)
-            try:
-                driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-            except:
-                print("□ 드라이버 오픈 에러.. 30초후 재시작합니다.")
-                time.sleep(30)
-                try:
-                    driver.close()
-                    time.sleep(5)
-                    driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-                except:
-                    driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
-            time.sleep(1)
-            wait, driver = driver_utils.naver_login(driver, wait, naver_id, naver_pw)  # 로그인 함수
-
-            # 블로그 클릭
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'service_icon.type_blog'))).click()
-            time.sleep(1)
-            # 글쓰기 클릭
-            driver.switch_to.window(driver.window_handles[-1])
-            wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
-            time.sleep(3)
-
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.switch_to.default_content()  # 기본 iframe으로 복귀
-            driver.switch_to.frame('mainFrame')
-
-        # 작성 중인 글이 있습니다
-        time.sleep(5)
-        try:
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-popup-button-text"))).click()
-            time.sleep(3)
-        except:
-            print("popup_exception")
-
-
-        # 복붙용 크롬창 열기 (실제로는 여기에 작성)
-        driver.switch_to.window(driver.window_handles[-2])
-        wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
-        time.sleep(3)
-
-
-        # 글쓰기 시작
-        driver.switch_to.window(driver.window_handles[-2])
-        time.sleep(1)
-        driver.switch_to.default_content()  # 기본 iframe으로 복귀
-        driver.switch_to.frame('mainFrame')
-
-        # 작성 중인 글이 있습니다
-        time.sleep(5)
-        try:
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-popup-button-text"))).click()
-            time.sleep(3)
-        except:
-            print("popup_exception")
-
-        # 도움말 있는지 체크
-        # 도움말이 있으면 발행버튼이 안눌림
-        if 'se-help-panel-close-button' in str(driver.page_source):
-            print('있다')
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-help-panel-close-button"))).click()
-
-
-        # 중요!!!! 브라우저 패널의 높이값
-        panel_height = driver.execute_script('return window.outerHeight - window.innerHeight;')
-
-        ### 제목 입력
-        # 제목 입력 element
-        # titel_input_box = driver.find_element(By.XPATH, """//span[text()='제목']""")
-        titel_input_box = wait.until(EC.presence_of_element_located((By.XPATH, """//span[text()='제목']""")))
-        # 제목 입력 element의 x 값 가져오기 (절대값)
-        abs_x = titel_input_box.location['x']
-        # 제목 입력 element의 y 값 가져오기 (상대값, 브라우저에 있는 y값)
-        y = titel_input_box.location['y']
-        abs_y = y + panel_height
-        # 마우스 이동해서 클릭
-        pyautogui.click(abs_x + 20, abs_y + 20)
-        time.sleep(1)
-        posting_utils.posting_run(post_title, image_folder_path, img_file_len, video_folder_path, video_file_len,
-                    imageLink_folder_path, imgLink_file_len, image_file_list, video_file_list, imageLink_file_list, videoLink_box, multi_img_folder_path, driver, wait, post_title, p_title)
-        time.sleep(3)
-
-        ### 본문 입력
-        # 본문 입력 element
-        pyautogui.press('hangul')
-        contents_input_box = driver.find_element(By.CLASS_NAME,"se-canvas-bottom")
-        # 본문 입력 element
-        panel_height = driver.execute_script('return window.outerHeight - window.innerHeight;')
-        # 본문 입력 element의 x 값 가져오기 (절대값)
-        abs_x = contents_input_box.location['x']
-        # 본문 입력 element의 y 값 가져오기 (상대값, 브라우저에 있는 y값)
-        y = contents_input_box.location['y']
-        abs_y = y + panel_height
-        pyautogui.click(abs_x + 1000, abs_y + 200)
-        time.sleep(3)
-        posting_utils.posting_run(original_post, image_folder_path, img_file_len, video_folder_path, video_file_len,
-                                  imageLink_folder_path, imgLink_file_len, image_file_list, video_file_list, imageLink_file_list, videoLink_box, multi_img_folder_path, driver, wait, post_title, p_title)
-
-        # 도움말 있는지 체크
-        # 도움말이 있으면 발행버튼이 안눌림
-        if 'se-help-panel-close-button' in str(driver.page_source):
-            print('있다')
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-help-panel-close-button"))).click()
-
-        # 발행
-        driver.switch_to.window(driver.window_handles[-2])
-        time.sleep(1)
-        driver.switch_to.default_content()  # 기본 iframe으로 복귀
-        driver.switch_to.frame('mainFrame')
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "publish_btn__m9KHH"))).click()
-        time.sleep(3)
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "confirm_btn__WEaBq"))).click()
-
-        print("발행 완료")
-
-        time.sleep(3)
-        for _ in range(len(driver.window_handles)):
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.close()
-            time.sleep(0.5)
-        time.sleep(3)
+        # ### 네이버 로그인
+        # try:
+        #     headless_checked = False
+        #     try:
+        #         driver = driver_utils.driversetting(headless_checked)
+        #         print("driver 세팅 완료")
+        #     except:
+        #         print("☆ Error driver setting...")
+        #         print("30초후 재개 됩니다.")
+        #         time.sleep(30)
+        #         driver = driver_utils.driversetting(headless_checked)
+        #
+        #     driver.maximize_window()
+        #     wait = WebDriverWait(driver, 10)
+        #     try:
+        #         driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #     except:
+        #         print("□ 드라이버 오픈 에러.. 30초후 재시작합니다.")
+        #         time.sleep(30)
+        #         try:
+        #             driver.close()
+        #             time.sleep(5)
+        #             driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #         except:
+        #             driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #     time.sleep(1)
+        #     wait, driver = driver_utils.naver_login(driver, wait, naver_id, naver_pw)  # 로그인 함수
+        #
+        #     # 블로그 클릭
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'service_icon.type_blog'))).click()
+        #     time.sleep(1)
+        #     # 글쓰기 클릭
+        #     driver.switch_to.window(driver.window_handles[-1])
+        #     wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
+        #     time.sleep(3)
+        #
+        #     driver.switch_to.window(driver.window_handles[-1])
+        #     driver.switch_to.default_content()  # 기본 iframe으로 복귀
+        #     driver.switch_to.frame('mainFrame')
+        # except:
+        #     try:
+        #         driver_utils.driver_close(driver)
+        #     except:
+        #         pass
+        #
+        #     headless_checked = False
+        #     try:
+        #         driver = driver_utils.driversetting(headless_checked)
+        #         print("driver 세팅 완료")
+        #     except:
+        #         print("☆ Error driver setting...")
+        #         print("30초후 재개 됩니다.")
+        #         time.sleep(30)
+        #         driver = driver_utils.driversetting(headless_checked)
+        #
+        #     driver.maximize_window()
+        #     wait = WebDriverWait(driver, 10)
+        #     try:
+        #         driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #     except:
+        #         print("□ 드라이버 오픈 에러.. 30초후 재시작합니다.")
+        #         time.sleep(30)
+        #         try:
+        #             driver.close()
+        #             time.sleep(5)
+        #             driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #         except:
+        #             driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+        #     time.sleep(1)
+        #     wait, driver = driver_utils.naver_login(driver, wait, naver_id, naver_pw)  # 로그인 함수
+        #
+        #     # 블로그 클릭
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'service_icon.type_blog'))).click()
+        #     time.sleep(1)
+        #     # 글쓰기 클릭
+        #     driver.switch_to.window(driver.window_handles[-1])
+        #     wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
+        #     time.sleep(3)
+        #
+        #     driver.switch_to.window(driver.window_handles[-1])
+        #     driver.switch_to.default_content()  # 기본 iframe으로 복귀
+        #     driver.switch_to.frame('mainFrame')
+        #
+        # # 작성 중인 글이 있습니다
+        # time.sleep(5)
+        # try:
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-popup-button-text"))).click()
+        #     time.sleep(3)
+        # except:
+        #     print("popup_exception")
+        #
+        #
+        # # 복붙용 크롬창 열기 (실제로는 여기에 작성)
+        # driver.switch_to.window(driver.window_handles[-2])
+        # wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='글쓰기']"))).click()
+        # time.sleep(3)
+        #
+        #
+        # # 글쓰기 시작
+        # driver.switch_to.window(driver.window_handles[-2])
+        # time.sleep(1)
+        # driver.switch_to.default_content()  # 기본 iframe으로 복귀
+        # driver.switch_to.frame('mainFrame')
+        #
+        # # 작성 중인 글이 있습니다
+        # time.sleep(5)
+        # try:
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-popup-button-text"))).click()
+        #     time.sleep(3)
+        # except:
+        #     print("popup_exception")
+        #
+        # # 도움말 있는지 체크
+        # # 도움말이 있으면 발행버튼이 안눌림
+        # if 'se-help-panel-close-button' in str(driver.page_source):
+        #     print('있다')
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-help-panel-close-button"))).click()
+        #
+        #
+        # # 중요!!!! 브라우저 패널의 높이값
+        # panel_height = driver.execute_script('return window.outerHeight - window.innerHeight;')
+        #
+        # ### 제목 입력
+        # # 제목 입력 element
+        # # titel_input_box = driver.find_element(By.XPATH, """//span[text()='제목']""")
+        # titel_input_box = wait.until(EC.presence_of_element_located((By.XPATH, """//span[text()='제목']""")))
+        # # 제목 입력 element의 x 값 가져오기 (절대값)
+        # abs_x = titel_input_box.location['x']
+        # # 제목 입력 element의 y 값 가져오기 (상대값, 브라우저에 있는 y값)
+        # y = titel_input_box.location['y']
+        # abs_y = y + panel_height
+        # # 마우스 이동해서 클릭
+        # pyautogui.click(abs_x + 20, abs_y + 20)
+        # time.sleep(1)
+        # posting_utils.posting_run(post_title, image_folder_path, img_file_len, video_folder_path, video_file_len,
+        #             imageLink_folder_path, imgLink_file_len, image_file_list, video_file_list, imageLink_file_list, videoLink_box, multi_img_folder_path, driver, wait, post_title, p_title)
+        # time.sleep(3)
+        #
+        # ### 본문 입력
+        # # 본문 입력 element
+        # pyautogui.press('hangul')
+        # contents_input_box = driver.find_element(By.CLASS_NAME,"se-canvas-bottom")
+        # # 본문 입력 element
+        # panel_height = driver.execute_script('return window.outerHeight - window.innerHeight;')
+        # # 본문 입력 element의 x 값 가져오기 (절대값)
+        # abs_x = contents_input_box.location['x']
+        # # 본문 입력 element의 y 값 가져오기 (상대값, 브라우저에 있는 y값)
+        # y = contents_input_box.location['y']
+        # abs_y = y + panel_height
+        # pyautogui.click(abs_x + 1000, abs_y + 200)
+        # time.sleep(3)
+        # posting_utils.posting_run(original_post, image_folder_path, img_file_len, video_folder_path, video_file_len,
+        #                           imageLink_folder_path, imgLink_file_len, image_file_list, video_file_list, imageLink_file_list, videoLink_box, multi_img_folder_path, driver, wait, post_title, p_title)
+        #
+        # # 도움말 있는지 체크
+        # # 도움말이 있으면 발행버튼이 안눌림
+        # if 'se-help-panel-close-button' in str(driver.page_source):
+        #     print('있다')
+        #     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "se-help-panel-close-button"))).click()
+        #
+        # # 발행
+        # driver.switch_to.window(driver.window_handles[-2])
+        # time.sleep(1)
+        # driver.switch_to.default_content()  # 기본 iframe으로 복귀
+        # driver.switch_to.frame('mainFrame')
+        # wait.until(EC.presence_of_element_located((By.CLASS_NAME, "publish_btn__m9KHH"))).click()
+        # time.sleep(3)
+        # wait.until(EC.presence_of_element_located((By.CLASS_NAME, "confirm_btn__WEaBq"))).click()
+        #
+        # print("발행 완료")
+        #
+        # time.sleep(3)
+        # for _ in range(len(driver.window_handles)):
+        #     driver.switch_to.window(driver.window_handles[-1])
+        #     driver.close()
+        #     time.sleep(0.5)
+        # time.sleep(3)
         try:
             vpn_utils.mvpn_close(dlg, dlg2)
+            input()
         except:
             print("mvpn 종료 에러")
             dlg['Button6'].click()  # 닫기 버튼 클릭
@@ -325,37 +326,24 @@ while True:
             time.sleep(1)
             print("mvpn 종료")
             time.sleep(1)
+            input()
         # try:
         #     vpn_utils.mvpn_close(dlg)
         # except:
         #     input("mvpn 종료 에러... 현재상태 캡쳐 부탁드리고, 터미널창에 엔터 입력후 ui가 종료되는지 체크 부탁드립니다.")
         # time.sleep(3)
 
-        minutes, seconds = utils.get_lab_time(start_time)
-        print(f"■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
-        downloaders.new_model.textBrowser.append(f"■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
-        logger.debug(f"{p_title}- ■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
+    #     minutes, seconds = utils.get_lab_time(start_time)
+    #     print(f"■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
+    #     downloaders.new_model.textBrowser.append(f"■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
+    #     logger.debug(f"{p_title}- ■ [{number}] {post_title} 포스팅 완료.  소요시간 : {minutes}분 {seconds}초")
+    #
+    # success_data_list = utils.get_log_data_for_success()
+    # success_data_list = list(set(success_data_list))
+    # # 모든 p_title이 완료되었는지 확인
+    # print(len(success_data_list))
+    # print(len(df))
+    # if len(success_data_list) == len(df):
+    #     print(f"■ 포스팅 모두 완료.")
+    #     break  # 모든 p_title이 완료되면 while 문 종료
 
-    success_data_list = utils.get_log_data_for_success()
-    success_data_list = list(set(success_data_list))
-    # 모든 p_title이 완료되었는지 확인
-    print(len(success_data_list))
-    print(len(df))
-    if len(success_data_list) == len(df):
-        print(f"■ 포스팅 모두 완료.")
-        break  # 모든 p_title이 완료되면 while 문 종료
-
-        # except Exception as e:
-        #     print(e)
-        #     logger.error(f"■ [{number}] {post_title} 포스팅 실패")
-        #     try:
-        #         for _ in range(len(driver.window_handles)):
-        #             driver.switch_to.window(driver.window_handles[-1])
-        #             driver.close()
-        #             time.sleep(0.5)
-        #         try:
-        #             vpn_utils.mvpn_close(dlg)
-        #         except:
-        #             pass
-        #     except:
-        #         pass
